1ce3a18bcc90aba853090bf7b1b7c34da275572f
FatimahMohamed <fatimah1pm@gmail.com>
2025-07-11 13:27:18 +0100

    Update blog admin settings, templates, and styles; add pagination and new base layout

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from django.views import generic
 from .models import Post
 
@@ -7,4 +8,9 @@ from .models import Post
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
-    template_name = "post_list.html"
+    template_name = "blog/index.html"
+    paginate_by = 6
+
+
+def my_blog(request):
+    return HttpResponse("Hello, blog!")
